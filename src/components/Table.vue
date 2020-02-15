9a4279b78827e60e429a1c37a261821f26d58791
Dziuperman <dziuperman@gmail.com>
2020-02-15 17:21:38 +1000

    Init commit

---
+++ src/components/Table.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="base-demo" style="width: 400px">
+        <vue-table-dynamic :params="params"></vue-table-dynamic>
+    </div>
+</template>
+
+<script>
+    import VueTableDynamic from 'vue-table-dynamic'
+
+    export default {
+        name: 'Demo',
+        data() {
+            return {
+                params: {
+                    data: [
+                        ['Cell-1', 'Cell-2', 'Cell-3'],
+                        ['Cell-4', 'Cell-5', 'Cell-6'],
+                        ['Cell-7', 'Cell-8', 'Cell-9']
+                    ]
+                }
+            }
+        },
+        components: { VueTableDynamic }
+    }
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueTableDynamic from 'vue-table-dynamic'
 
-Vue.config.productionTip = false
+Vue.use(VueTableDynamic);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
