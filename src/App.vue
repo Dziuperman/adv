152c4204175b1e0e7d2604981200c82978234fd1
Dziumesiuk <dziu1996@gmail.com>
2020-02-17 17:50:35 +1000

    feat: created adverts table from api, created api methods

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
+        <img alt="Logotype" class="askom-logo" src="http://xn--80aughn.xn--p1ai/local/templates/askom/images/logo.png">
+
         <table-component></table-component>
     </div>
 </template>
 
 <script>
     import TableComponent from './components/Table'
+
     export default {
         name: 'App',
         components: {
@@ -23,5 +25,20 @@
         text-align: center;
         color: #2c3e50;
         margin-top: 60px;
+    }
+
+    .askom-logo {
+        position: relative;
+
+        object-fit: contain;
+        object-position: center center;
+
+        background: #d32f2f;
+        border-radius: 6px;
+        border-bottom: 0;
+        padding: 0.25em 0.2em 0.25em 0.2em;
+
+        width: 250px;
+        max-width: 100%;
     }
 </style>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,37 +1,45 @@
 <template>
     <v-container>
         <v-app id="inspire">
+            <div>
+                <v-select
+                        v-model="selectedAccount"
+                        :items="accounts"
+                        label="Выберите аккаунт"
+                />
+            </div>
 
             <v-card-title>
-                Nutrition
+                Список транзакций
                 <v-spacer></v-spacer>
                 <v-text-field
-                    v-model="search"
-                    append-icon="search"
-                    label="Поиск"
-                    single-line
-                    hide-details
+                        v-model="search"
+                        append-icon="search"
+                        label="Поиск"
+                        single-line
+                        hide-details
                 ></v-text-field>
             </v-card-title>
 
             <v-data-table
-                :headers="headers"
-                :items="groups"
-                :search="search"
-                :expanded.sync="expanded"
-                hide-default-footer
-                item-key="id"
-                show-expand
-                class="elevation-1"
+                    :headers="headers"
+                    :items="adverts"
+                    :search="search"
+                    :expanded.sync="expanded"
+                    hide-default-footer
+                    item-key="id"
+                    show-expand
+                    class="elevation-1"
+                    :loading="busy"
             >
 
                 <template v-slot:expanded-item="{ headers, item }">
                     <td :colspan="headers.length">
                         <v-data-table
-                        :items="item.events"
-                        :headers="eventHeaders"
-                        item-key="id"
-                        hide-default-footer
+                                :items="item.events"
+                                :headers="eventHeaders"
+                                item-key="id"
+                                hide-default-footer
                         >
                         </v-data-table>
                     </td>
@@ -52,133 +60,90 @@
                     added_by: '',
                 },
                 search: '',
-                errors: [],
                 expanded: [],
                 headers: [
                     {
                         text: 'Название',
                         align: 'left',
-                        value: 'name',
+                        value: 'title',
                     },
-                    {text: 'id объявления', value: 'id'},
-                    {text: 'Адрес', value: 'address.street'},
+                    {text: 'id объявления', value: 'id'}
                 ],
                 eventHeaders: [
                     {
-                        text: 'Дата', value: 'data'
+                        text: 'Дата', value: 'date'
                     },
                     {
-                        text: 'Событие', value: 'event'
+                        text: 'Событие', value: 'message'
                     },
                     {
                         text: 'Сумма', value: 'amount'
                     },
                 ],
-                adverts: [
-                    {
-                        id: 1,
-                        name: 'name-1',
-                        data: '1.1',
-                        event: 'P',
-                        amount: '2 000'
-                    },
-                    {
-                        id: 1,
-                        name: 'name-1',
-                        data: '1.2',
-                        event: 'PO',
-                        amount: '2 000'
-                    },
-                    {
-                        id: 1,
-                        name: 'name-1',
-                        data: '1.3',
-                        event: 'P',
-                        amount: '2 000'
-                    },
-                    {
-                        id: 2,
-                        name: 'name-2',
-                        data: '1.4',
-                        event: 'V',
-                        amount: '2 000'
-                    },
-                    {
-                        id: 1,
-                        name: 'name-1',
-                        data: '1.5',
-                        event: 'P',
-                        amount: '2 000'
-                    },
-                    {
-                        id: 1,
-                        name: 'name-1',
-                        data: '1.6',
-                        event: 'PO',
-                        amount: '2 000'
-                    },
-                    {
-                        id: 2,
-                        name: 'name-2',
-                        data: '1.7',
-                        event: 'PO',
-                        amount: '2 000'
-                    },
-                    {
-                        id: 1,
-                        name: 'name-1',
-                        data: '1.8',
-                        event: 'V',
-                        amount: '2 000'
-                    },
-                    {
-                        id: 1,
-                        name: 'name-1',
-                        data: '1.9',
-                        event: 'P',
-                        amount: '2 000'
-                    },
-
-                ],
-                groups: [],
-                foodType: null,
+                accounts: [],
+                selectedAccount: null,
+                apiDomain: 'http://192.168.1.36:8000/io.askom.farpost_statistics',
+                adverts: [],
+                busy: true,
             }
+        },
+        beforeMount() {
+            this.fetchAccounts()
+            // this.fetchAdvertisements()
         },
         mounted() {
-            axios.get(`http://jsonplaceholder.typicode.com/todos`)
-                .then(response => {
-                    // JSON responses are automatically parsed.
-                    this.todos = response.data;
-                    console.log(this.todos);
-                })
-                .catch(e => {
-                    this.errors.push(e)
-                });
-            axios.get(`https://jsonplaceholder.typicode.com/users`)
-                .then(response => {
-                    this.users = response.data
-                })
-                .catch(e => {
-                    this.errors.push(e)
-                });
 
-            for (let element of this.adverts) {
-                let existingGroups = this.groups.filter(group => group.id == element.id);
-                if (existingGroups.length > 0) {
-                    existingGroups[0].events.push(element);
-                }
-                else {
-                    let newGroup = {
-                        id: element.id,
-                        name: element.name,
-                        events: [element],
-
-                    };
-                    this.groups.push(newGroup);
-                }
-            }
-            console.log(this.groups);
         },
+        methods: {
+            fetchAccounts() {
+                this.busy = true
+
+                axios.get(this.apiDomain + '/api.php?method=getAccounts')
+                    .then(resposne => {
+                        this.accounts = resposne.data
+                        this.busy = false
+                    }).catch(e => {
+                    console.log('Error while fetching accounts', e)
+                })
+            },
+            fetchAdvertisements() {
+                if (!this.selectedAccount) return false
+
+                this.busy = true
+                axios.get(this.apiDomain + '/api.php?method=list&account=' + this.selectedAccount)
+                    .then(response => {
+                        this.adverts = []
+                        this.groupTransactions(response.data, () => {
+                            this.busy = false
+                        })
+                    })
+                    .catch(e => {
+                        console.error(e)
+                    })
+            },
+            async groupTransactions(transactions, callback) {
+                await transactions.map((transaction) => {
+                    const existGroup = this.adverts.findIndex(advertGroup => advertGroup.id === transaction.advertisement_id)
+
+                    if (existGroup !== -1) {
+                        this.adverts[existGroup].events.push(transaction)
+                    } else {
+                        this.adverts.push({
+                            id: transaction.advertisement_id,
+                            title: transaction.advertisement_title,
+                            events: [transaction]
+                        })
+                    }
+                })
+
+                if (typeof callback === "function") callback()
+            }
+        },
+        watch: {
+            selectedAccount() {
+                return this.fetchAdvertisements()
+            }
+        }
     }
 
 </script>
