649c3f4eba22f9a8b673fc16b4a224cbdb29072f
Dziumesiuk <dziu1996@gmail.com>
2020-02-18 14:14:00 +1000

    feat: добавлена обработка и отправка даты

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,6 +3,8 @@
         <v-app id="inspire">
             <div>
                 <v-select
+                        :disabled="!loaded"
+                        :loading="!loaded"
                         v-model="selectedAccount"
                         :items="accounts"
                         label="Выберите аккаунт"
@@ -11,8 +13,9 @@
                 <v-layout row wrap>
                     <v-flex xs12 sm6 md4>
                         <v-menu
+                                :disabled="!loaded"
                                 v-model="menu1"
-                                :close-on-content-click="true"
+                                :close-on-content-click="false"
                                 :nudge-right="40"
                                 lazy
                                 transition="scale-transition"
@@ -22,38 +25,42 @@
                         >
                             <template v-slot:activator="{ on }">
                                 <v-text-field
-                                        v-model="date"
+                                        :disabled="!loaded"
+                                        v-model="dateFrom"
                                         label="Picker without buttons"
                                         prepend-icon="event"
                                         readonly
                                         v-on="on"
                                 ></v-text-field>
                             </template>
-                            <v-date-picker v-model="date" @input="menu1 = false"></v-date-picker>
+                            <v-date-picker v-model="dateFrom" @input="menu1 = false"></v-date-picker>
                         </v-menu>
                     </v-flex>
                     <v-spacer></v-spacer>
                     <v-flex xs12 sm6 md4>
                         <v-menu
                                 v-model="menu2"
-                                :close-on-content-click="true"
+                                :disabled="!loaded"
+                                :close-on-content-click="false"
                                 :nudge-right="40"
                                 lazy
                                 transition="scale-transition"
                                 offset-y
                                 full-width
                                 min-width="290px"
+
                         >
                             <template v-slot:activator="{ on }">
                                 <v-text-field
-                                        v-model="date"
+                                        :disabled="!loaded"
+                                        v-model="dateTo"
                                         label="Picker without buttons"
                                         prepend-icon="event"
                                         readonly
                                         v-on="on"
                                 ></v-text-field>
                             </template>
-                            <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+                            <v-date-picker v-model="dateTo" @input="menu2 = false"></v-date-picker>
                         </v-menu>
                     </v-flex>
                 </v-layout>
@@ -68,11 +75,15 @@
                         append-icon="search"
                         label="Поиск"
                         single-line
-                        hide-details
+                        :hide-details="true"
+                        :disabled="!loaded"
                 ></v-text-field>
             </v-card-title>
 
             <v-data-table
+                    :disable-filtering="!loaded"
+                    :disable-sort="!loaded"
+                    :no-data-text="noDataText"
                     :headers="headers"
                     :items="grouppedTransacion"
                     :search="search"
@@ -81,8 +92,15 @@
                     item-key="id"
                     show-expand
                     class="elevation-1"
-                    :loading="busy"
+                    :loading="busy && loaded"
             >
+
+                <template v-slot:item.id="{ item }">
+                    <a :href="'https://farpost.ru/' + item.id" noreferrer target="_blank" v-if="item.id">
+                        №
+                        <span style="user-select: all">{{item.id}}</span>
+                    </a>
+                </template>
 
                 <template v-slot:expanded-item="{ headers, item }">
                     <td :colspan="headers.length">
@@ -106,11 +124,10 @@
     export default {
         data() {
             return {
-                date: new Date().toISOString().substr(0, 10),
                 menu1: false,
                 menu2: false,
-                dateFrom: '',
-                dateTo: '',
+                dateFrom: new Date().toISOString().substr(0, 10),
+                dateTo: new Date().toISOString().substr(0, 10),
                 filters: {
                     search: '',
                     added_by: '',
@@ -123,7 +140,7 @@
                         align: 'left',
                         value: 'title',
                     },
-                    {text: 'id объявления', value: 'id'}
+                    {text: 'Номер объявления', value: 'id'}
                 ],
                 eventHeaders: [
                     {
@@ -140,6 +157,7 @@
                 selectedAccount: null,
                 apiDomain: 'http://192.168.1.36:8000/io.askom.farpost_statistics',
                 advertisements: [],
+                loaded: false,
                 busy: true,
             }
         },
@@ -154,6 +172,7 @@
                     .then(resposne => {
                         this.accounts = resposne.data
                         this.busy = false
+                        this.loaded = true
                     }).catch(e => {
                     console.log('Error while fetching accounts', e)
                 })
@@ -162,29 +181,32 @@
                 if (!this.selectedAccount) return false
 
                 this.busy = true
-                axios.get(this.apiDomain + '/api.php?method=list&account=' + this.selectedAccount)
-                    .then(response => {
-                        this.advertisements = response.data.transactions
+                axios.get(this.apiDomain + '/api.php?method=list&account=' +
+                    this.selectedAccount + '&from=' + this.formattedDateFrom + '&to=' + this.formattedDateTo
+                ).then(response => {
+                    this.advertisements = response.data.transactions
 
-                        this.busy = false
-                    })
-                    .catch(e => {
-                        console.error(e)
-                    })
+                    this.busy = false
+                }).catch(e => {
+                    console.error(e)
+                })
             },
             parseDate(date) {
                 if (!date) return null
 
-                const [day, month, year] = date.split('.')
-                return `${day.padStart(2, '0')}-${month.padStart(2, '0')}-${year}`
+                const [year, month, day] = date.split('-')
+                return `${day}.${month}.${year}`
             },
-            getDate() {
-                console.log(this.date)
-            }
         },
         watch: {
             selectedAccount() {
-                return this.fetchAdvertisements()
+                this.fetchAdvertisements()
+            },
+            dateFrom() {
+                this.fetchAdvertisements()
+            },
+            dateTo() {
+                this.fetchAdvertisements()
             }
         },
         computed: {
@@ -197,6 +219,10 @@
                     if (existGroup !== -1) {
                         grouppedTransacion[existGroup].events.push(transaction)
                     } else {
+                        if (!transaction.advertisement_title) {
+                            transaction.advertisement_title = '[Пополнения баланса]'
+                        }
+
                         grouppedTransacion.push({
                             id: transaction.advertisement_id,
                             title: transaction.advertisement_title,
@@ -206,10 +232,23 @@
                 })
 
                 return grouppedTransacion
+            },
+            formattedDateFrom() {
+                return this.parseDate(this.dateFrom)
+            },
+            formattedDateTo() {
+                return this.parseDate(this.dateTo)
+            },
+            noDataText() {
+                if (!this.loaded) {
+                    return 'Соединение с сервером...'
+                } else if (!this.selectedAccount) {
+                    return 'Выберите аккаунт'
+                } else {
+                    return 'Транзакции не найдены!'
+                }
             }
+
         }
-
-
     }
-
 </script>
